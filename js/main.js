68e091059aa90528c46304e779da74528bfff69a
Zuevskaya1985 <az2711710@gmail.com>
2019-09-25 10:19:28 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.slider-slide').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-slide',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+                  
+  
